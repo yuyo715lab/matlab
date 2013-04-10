90a05bdfceed2794e26c8c2dc9c110b3684dbcf5
yuyo715lab <yuyo.jushin@googlemail.com>
2013-04-10 14:23:51 +0900

    pvcurve upper

--- PVcurve/main.m
+++ PVcurve/main.m
@@ -1,8 +1,10 @@
-function [P,Q,RHO,THEATA] = main(Y)
+function [P,Q,RHO,THEATA] = main(N,Ref,PQorPV,NonRef,R,Tr,e,f,Vs,V,dV,Ps,Qs,PQ)
 
-%============= Define.m ================
-[N,Ref,PQorPV,NonRef,R,Tr,e,f,Vs,V,dV,Ps,Qs,PQ] = Define();
-%============= Define.m ================
+
+
+%============= admittance.m ================
+Y = admittance(N,R,Tr);
+%============= admittance.m ================
 
 %============= PandQ.m ================
 [dP,dQ,dV,P,Q,V] = PandQ(N,Y,PQ,Ps,Qs,Vs,e,f);
--- PVcurve/pvcurve.m
+++ PVcurve/pvcurve.m
@@ -1,32 +1,55 @@
-%============= Define.m ================
-[N,Ref,PQorPV,NonRef,R,Tr,e,f,Vs,V,dV,Ps,Qs,PQ] = Define();
-%============= Define.m ================
-
-%============= admittance.m ================
-Y = admittance(N,R,Tr);
-%============= admittance.m ================
-
-
-[P,Q,RHO,THEATA] = main(Y);
-Y0 = Y;
-
-
 %######### specified node #########
 SN = 5;
 %######### specified node #########
 
-%######### Cycle Number #########
-CN = 20;
-%######### Cycle Number #########
 
-RHO(SN)
--P(SN)
+%######### First PQ-proportion #########
+fprop = 0;
+%######### First PQ-proportion #########
 
-for k = 1:CN
-  Y = Y0;
-  Y(SN,SN) = Y0(SN,SN) + (0.8 + 0.3*(k-1))*(-P(SN) - Q(SN)*i) / (RHO(SN)^2);
-  [P,Q,RHO,THEATA] = ...
-      main(Y);
-  RHO(SN) 
- - P(SN)
+%######### Delta propotion #########
+dprop = 0.01;
+%######### Delta proportion #########
+
+%######### P limit #########
+Plimit = 4.06;
+%######### P limit #########
+
+%============= Define.m ================
+[N,Ref,PQorPV,NonRef,R,Tr,e,f,Vs,V,dV,Ps,Qs,PQ,Pss] = Define(1,SN);
+%============= Define.m ================
+
+CN = 0;
+prop = fprop;
+while -Pss * prop < Plimit
+  CN = CN + 1;
+  prop = prop + dprop;
 end
+
+
+XY = zeros(2,CN);
+
+k = 1;
+prop = fprop;
+while -Pss * prop < Plimit
+
+  %============= Define.m ================
+  [N,Ref,PQorPV,NonRef,R,Tr,e,f,Vs,V,dV,Ps,Qs,PQ,Pss] = Define(prop,SN);
+  %============= Define.m ================
+
+  [P,Q,RHO,THEATA] = ...
+      main(N,Ref,PQorPV,NonRef,R,Tr,e,f,Vs,V,dV,Ps,Qs,PQ);
+  XY(1,k) = -P(SN);
+  XY(2,k) = RHO(SN);
+  prop = prop + dprop;
+  k = k + 1;
+end
+
+plot(XY(1,:),XY(2,:))
+
+
+
+
+
+
+
